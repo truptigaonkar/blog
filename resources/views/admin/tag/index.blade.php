e68506b8f81bb3b9b7ca4590dccc931cad0cf503
truptigaonkar <trups11.trupti@gmail.com>
2018-03-15 19:55:02 +0100

    Bug fix

--- resources/views/admin/tag/index.blade.php
+++ resources/views/admin/tag/index.blade.php
@@ -1,7 +1,12 @@
 @extends('admin.app')
 
 @section('headSection')
-<link rel="stylesheet" href="{{ asset('admin/plugins/datatables/dataTables.bootstrap.css') }}">
+<!-- DataTables -->
+<link rel="stylesheet" href="{{ asset('admin/bower_components/datatables.net-bs/css/dataTables.bootstrap.css') }}">
+<!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+<link rel="stylesheet" href="{{ asset('admin/dist/css/skins/_all-skins.min.css') }}">
+  
 @endsection
 @section('main-content')
   <!-- Content Wrapper. Contains page content -->
